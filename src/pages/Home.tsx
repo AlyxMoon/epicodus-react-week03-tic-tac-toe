0fdefe91f77bfbca9d6f26450b95cd50cee29d32
AlyxMoon <allisterkmoon@gmail.com>
2021-09-15 09:06:50 -0700

    add some intro text to the homepage

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,6 @@
 import { PureComponent } from 'react'
 import { connect, ConnectedProps } from 'react-redux'
+import { NavLink } from 'react-router-dom'
 
 type Props = {}
   & PropsFromRedux
@@ -9,6 +10,12 @@ class HomePage extends PureComponent<Props> {
     return (
       <>
         <h1>Home Page</h1>
+
+        <NavLink
+          to='/game'
+        >Start a Game!</NavLink>
+
+        <p>This is ultimate tic-tac-toe. If you don't know about it, <a href='https://en.wikipedia.org/wiki/Ultimate_tic-tac-toe' target="_blank">you can find out more info here</a>.</p>
       </>
     )
   }
