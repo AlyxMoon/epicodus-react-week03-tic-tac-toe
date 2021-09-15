695d7cca105f5543d16e3cecb4be48e7040e6ee7
AlyxMoon <allisterkmoon@gmail.com>
2021-09-15 08:35:45 -0700

    set up project with typescript and redux

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { PureComponent } from 'react'
+
+interface Props {}
+
+export class App extends PureComponent<Props> {
+  public render () {
+    return (
+      <>
+        <h1>App</h1>
+      </>
+    )
+  }
+}
+
+export default App
